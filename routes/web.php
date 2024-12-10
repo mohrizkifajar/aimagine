b74fef2ddec75d408f38b4833455a3cd1a003d47
Moh. Rizki Fajar <mohrizkifajar@gmail.com>
2024-12-10 22:36:53 +0700

    Layouting

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ImageController;
 
 // Auth Pages
 Route::get('/sign-in', [AuthController::class, 'signIn'])->name('auth.sign-in');
 
 // Landing Pages
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+// Route Dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::get('/images', [ImageController::class, 'index'])->name('dashboard.images.index');
